14306cc9dee9db1d3c348289064793adf3b0af33
pblackman <ptblackman@gmail.com>
2024-02-24 09:20:07 -0500

    Rewrote linker code to avoid allocating and storing unnecessary data.  It now keeps the bb.asm file open while linking things.

--- linker.c
+++ linker.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // This reads the includes file created by bB and builds the
 // final assembly that will be sent to DASM.
@@ -19,70 +20,71 @@
 void link_files(char *path) {
     FILE *includesfile;
     FILE *asmfile;
-    char ***readbBfile;
+    FILE *bbfile;
+
     char prepend[500];
     char line[500];
     char asmline[500];
-    int bB = 2;            // part of bB file
-    int writebBfile[17] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int i;
-    int j;
 
+    bool readNextFile;
 
     if ((includesfile = fopen("includes.bB", "r")) == NULL)    // open file
     {
         fprintf(stderr, "Cannot open includes.bB for reading\n");
         exit(1);
     }
-    readbBfile = (char ***) malloc(sizeof(char **) * 17);
 
-//  while (fgets(line,500,includesfile))
-    while (1) {
-        if (fscanf(includesfile, "%s", line) == EOF)
-            break;
-        if ((!strncmp(line, "bB", 2)) && (line[2] != '.')) {
-            i = (int) (line[2]) - 48;
-            for (j = 1; j < writebBfile[i]; ++j)
-                printf("%s", readbBfile[i][j]);
-            continue;
+    if ((bbfile = fopen("bB.asm", "r")) == NULL) {
+        fprintf(stderr, "Error reading bB file!");
+        exit(1);
+    }
+
+    // read line of include file (essentially the build / make file)
+    while (fgets(line, 500, includesfile)) {
+
+        // trim off eol character.
+        int line_len = strlen(line);
+        if (line[line_len-1] == '\n') line[line_len-1] = 0;
+
+        // check if including a portion of the bB.asm code.
+        bool isBbFile = (!strncmp(line, "bB", 2)) && (line[2] == '.' || line[3] == '.');
+
+        if (isBbFile) {
+            readNextFile = false;
+
+            // read bb assembly source file until EOF or split point
+            while ((fgets(asmline, 500, bbfile) != NULL) && !readNextFile) {
+
+                // check for last bank split point within the bB source file
+                if (!strncmp(asmline, "; bB.asm file is split here", 20)) {
+                    readNextFile = true;
+                } else {
+                    printf("%s", asmline);
+                }
+            }
+
         } else {
-            strcpy(prepend, path);
-            strcat(prepend, line);
+            // reading from another file
             if ((asmfile = fopen(line, "r")) == NULL)    // try file w/o includes path
             {
+                strcpy(prepend, path);
+                strcat(prepend, line);
                 if ((asmfile = fopen(prepend, "r")) == NULL)    // open file
                 {
                     fprintf(stderr, "Cannot open %s for reading: %s\n", line, prepend);
                     exit(1);
                 }
-            } else if (strncmp(line, "bB\0", 2)!=0) {
+            } else if (strncmp(line, "bB\0", 2) != 0) {
                 fprintf(stderr, "User-defined %s found in current directory\n", line);
             }
-        }
-        while (fgets(asmline, 500, asmfile)) {
 
-            // check for last bank split point within the bB source file
-            if (!strncmp(asmline, "; bB.asm file is split here", 20)) {
-
-                writebBfile[bB]++;  // flag the split
-
-                readbBfile[bB] = (char **) malloc(sizeof(char *) * 50000);
-                readbBfile[bB][writebBfile[bB]] = (char *) malloc(strlen(line) + 3);
-
-                sprintf(readbBfile[bB][writebBfile[bB]], ";%s\n", line);
-            }
-            if (!writebBfile[bB])
+            // read from an included file
+            while (fgets(asmline, 500, asmfile) != NULL) {
                 printf("%s", asmline);
-            else {
-                readbBfile[bB][++writebBfile[bB]] = (char *) malloc(strlen(asmline) + 3);
-                sprintf(readbBfile[bB][writebBfile[bB]], "%s", asmline);
             }
+            fclose(asmfile);
         }
-        fclose(asmfile);
 
-        // if file has been split at this point, move to next flag index
-        if (writebBfile[bB])
-            bB++;
-        // if (writebBfile) fclose(bBfile);
     }
+    if (bbfile) fclose(bbfile);
 }
